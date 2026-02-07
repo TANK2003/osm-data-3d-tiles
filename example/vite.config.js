ce2a8319019f67932439e2e08abdcd5e542a95a6
karl_tayou <karl@vingtcinq.io>
2026-02-07 14:29:43 +0100

    Update Vite configuration to set base path for OSM data 3D tiles

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
-    base: "/",
+    base: "/osm-data-3d-tiles/",
     build: {
         rollupOptions: {
             input: {
